f3e1fe3d8d8c8c1e7739532c4b4b50c4529ca99c
Seb Jones <jonesseb100@gmail.com>
2025-08-01 10:08:27 +0100

    Add functions for returning peak powers rather than just amplitudes

--- src/PowerSpectrumCalculator.py
+++ src/PowerSpectrumCalculator.py
@@ -12,7 +12,8 @@ from src.QTNMTraps import HarmonicTrap, BathtubTrap
 from src.Particle import Particle
 import numpy as np
 import scipy.constants as sc
-from scipy.special import jv
+from scipy.special import jv, jvp, j1
+from scipy.integrate import quad
 
 
 class PowerSpectrumCalculator:
@@ -87,3 +88,58 @@ class PowerSpectrumCalculator:
             return (amp1, amp2)
         else:
             raise TypeError("Trap type currently not supported")
+
+    def GetPowerNorm(self):
+        """
+        Calculate the power normalisation for the electron in this case
+
+        Returns
+        -------
+        float
+            Power factor in Watts
+        """
+
+        def alphaIntegrand(rho, kc):
+            """
+            Integrand for alpha calculation
+            """
+            return rho * ((j1(kc * rho) / (kc * rho))**2 + jvp(1, kc * rho)**2)
+
+        kc = 1.841 / self.__waveguide.wgR
+        alpha, _ = quad(alphaIntegrand, 0, self.__waveguide.wgR, args=kc)
+        r_gyro = np.sqrt(self.__particle.GetPosition()[
+                         0]**2 + self.__particle.GetPosition()[1]**2)
+
+        prefactor = self.__waveguide.CalcTE11Impedance(self.__trap.CalcOmega0(
+            self.__particle.GetSpeed(), self.__particle.GetPitchAngle())) * (sc.e * self.__particle.GetSpeed())**2 / (8 * np.pi * alpha)
+
+        if r_gyro == 0:
+            # Handle the special case where r_gyro = 0
+            # lim(x->0) j1(x)/x = 1/2, so (j1(x)/x)^2 = 1/4
+            return prefactor * (jvp(1, kc * r_gyro)**2 + 0.25)
+        else:
+            return prefactor * (jvp(1, kc * r_gyro)**2 + (j1(kc * r_gyro) / (kc * r_gyro))**2)
+
+    def GetSpectrumPowers(self, order: int):
+        """
+        Calculates the power in a given order sideband
+
+        Parameters
+        ----------
+        order : int
+            Order of sideband
+
+        Returns
+        -------
+        tuple
+            Power in sidebands of order 'order' in Watts
+        """
+        if not isinstance(order, int):
+            raise TypeError("Order must be an integer")
+        if order < 0:
+            raise TypeError("Order must be positive")
+        if not np.isfinite(order):
+            raise ValueError("Order must be finite")
+
+        a1, a2 = self.GetPeakAmp(order)
+        return (a1**2 * self.GetPowerNorm(), a2**2 * self.GetPowerNorm())
