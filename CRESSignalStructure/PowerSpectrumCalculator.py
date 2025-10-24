20ede345a96544203401a9a6a1cd317fdcd606ea
Seb Jones <jonesseb100@gmail.com>
2025-10-24 15:23:58 +0100

    Updates PowerSpectrumCalculator to inherit from BaseSpectrumCalculator

--- CRESSignalStructure/PowerSpectrumCalculator.py
+++ CRESSignalStructure/PowerSpectrumCalculator.py
@@ -10,82 +10,80 @@ from CRESSignalStructure.BaseTrap import BaseTrap
 from CRESSignalStructure.CircularWaveguide import CircularWaveguide
 from CRESSignalStructure.QTNMTraps import HarmonicTrap, BathtubTrap
 from CRESSignalStructure.Particle import Particle
+from CRESSignalStructure.BaseSpectrumCalculator import BaseSpectrumCalculator
 import numpy as np
 import scipy.constants as sc
 from scipy.special import jv, jvp, j1
 from scipy.integrate import quad
+from numpy.typing import ArrayLike, NDArray
 
 
-class PowerSpectrumCalculator:
+class PowerSpectrumCalculator(BaseSpectrumCalculator):
     def __init__(self, trap: BaseTrap, waveguide: CircularWaveguide,
                  particle: Particle):
+        super().__init__(trap, waveguide, particle)
         self.__trap = trap
         self.__waveguide = waveguide
         self.__particle = particle
 
-    def GetPeakFrequency(self, order: int):
+    def GetPeakFrequency(self, order: ArrayLike) -> NDArray:
         """
         Calculate the frequencies at which the components occur
 
         Parameters
         ----------
-        order : int
+        order : ArrayLike
             The order of the peak to calculate the frequency for
 
         Returns
         -------
-        tuple(float, float)
-            The frequency or frequencies at which components are observed in
-            radians/s
+        NDArray
+            The frequency/frequencies at which components are observed in Hertz
         """
-        if not isinstance(order, int):
+        order = np.asarray(order)
+        if not np.issubdtype(order.dtype, np.integer):
             raise TypeError("Order must be an integer")
-        if order < 0:
-            raise TypeError("Order must be positive")
-        if not np.isfinite(order):
+        if not np.any(np.isfinite(order)):
             raise ValueError("Order must be finite")
 
-        # Treat the n = 0 case separately
         v0 = self.__particle.GetSpeed()
         pa = self.__particle.GetPitchAngle()
-        return ((self.__trap.CalcOmega0(v0, pa) + order * self.__trap.CalcOmegaAxial(v0, pa)) / (2*np.pi),
-                (self.__trap.CalcOmega0(v0, pa) - order * self.__trap.CalcOmegaAxial(v0, pa)) / (2*np.pi))
+        f0 = self.__trap.CalcOmega0(v0, pa) / (2 * np.pi)
+        fa = self.__trap.CalcOmegaAxial(v0, pa) / (2*np.pi)
+        return f0 + order * fa
 
-    def GetPeakAmp(self, order: int):
+    def GetPeakAmp(self, order: ArrayLike) -> NDArray:
         """
         Calculate the peak amplitude in the sidebands for a given peak order
 
         Parameters
         ----------
-        order : int
-            Order of sideband
+        order : ArrayLike
+            Order of peak to calculate for
 
         Returns
         -------
-        tuple(float, float)
-            Peak amplitudes
+        NDArray
+            Complex peak amplitudes
         """
-        if not isinstance(order, int):
+        order = np.asarray(order)
+        if not np.issubdtype(order.dtype, np.integer):
             raise TypeError("Order must be an integer")
-        if order < 0:
-            raise TypeError("Order must be positive")
-        if not np.isfinite(order):
+        if not np.any(np.isfinite(order)):
             raise ValueError("Order must be finite")
 
         kc = 1.841 / self.__waveguide.wgR  # Consider only TE11 mode
-        f1, f2 = self.GetPeakFrequency(order)
+        f1 = self.GetPeakFrequency(order)
         pitchAngle = self.__particle.GetPitchAngle()
         v0 = self.__particle.GetSpeed()
         zmax = self.__trap.CalcZMax(pitchAngle)
-        beta1 = np.sqrt((f1 * 2 * np.pi / sc.c)**2 - kc**2)
-        beta2 = np.sqrt((f2 * 2 * np.pi / sc.c)**2 - kc**2)
+        beta = np.sqrt((f1 * 2 * np.pi / sc.c)**2 - kc**2)
 
         if isinstance(self.__trap, HarmonicTrap):
             q = self.__trap.Calcq(v0, pitchAngle)
             MArr = np.arange(-6, 7, 1)
-            amp1 = np.sum(jv(MArr, q) * jv(order - 2 * MArr, beta1 * zmax))
-            amp2 = np.sum(jv(MArr, q) * jv(-order - 2 * MArr, beta2 * zmax))
-            return (amp1, amp2)
+            amp = np.sum(jv(MArr, q) * jv(order - 2 * MArr, beta * zmax))
+            return amp
         elif isinstance(self.__trap, BathtubTrap):
             omega_a = v0 * np.sin(pitchAngle) / self.__trap.GetL0()
             t1 = self.__trap.CalcT1(v0, pitchAngle)
@@ -185,66 +183,9 @@ class PowerSpectrumCalculator:
                 return (E + F + G + H) / T
 
             secondSum = np.arange(-20, 21, 1)
-            amp1 = np.sum(CalcAlpha_n(secondSum) *
-                          CalcBeta_n(order - secondSum, beta1))
-            amp2 = np.sum(CalcAlpha_n(secondSum) *
-                          CalcBeta_n(-order - secondSum, beta2))
-            return (amp1, amp2)
+            amp = np.sum(CalcAlpha_n(secondSum) *
+                         CalcBeta_n(order - secondSum, beta))
+            return amp
 
         else:
             raise TypeError("Trap type currently not supported")
-
-    def GetPowerNorm(self):
-        """
-        Calculate the power normalisation for the electron in this case
-
-        Returns
-        -------
-        float
-            Power factor in Watts
-        """
-
-        def alphaIntegrand(rho, kc):
-            """
-            Integrand for alpha calculation
-            """
-            return rho * ((j1(kc * rho) / (kc * rho))**2 + jvp(1, kc * rho)**2)
-
-        kc = 1.841 / self.__waveguide.wgR
-        alpha, _ = quad(alphaIntegrand, 0, self.__waveguide.wgR, args=kc)
-        r_gyro = np.sqrt(self.__particle.GetPosition()[
-                         0]**2 + self.__particle.GetPosition()[1]**2)
-
-        prefactor = self.__waveguide.CalcTE11Impedance(self.__trap.CalcOmega0(
-            self.__particle.GetSpeed(), self.__particle.GetPitchAngle())) * (sc.e * self.__particle.GetSpeed())**2 / (8 * np.pi * alpha)
-
-        if r_gyro == 0:
-            # Handle the special case where r_gyro = 0
-            # lim(x->0) j1(x)/x = 1/2, so (j1(x)/x)^2 = 1/4
-            return prefactor * (jvp(1, kc * r_gyro)**2 + 0.25)
-        else:
-            return prefactor * (jvp(1, kc * r_gyro)**2 + (j1(kc * r_gyro) / (kc * r_gyro))**2)
-
-    def GetSpectrumPowers(self, order: int):
-        """
-        Calculates the power in a given order sideband
-
-        Parameters
-        ----------
-        order : int
-            Order of sideband
-
-        Returns
-        -------
-        tuple
-            Power in sidebands of order 'order' in Watts
-        """
-        if not isinstance(order, int):
-            raise TypeError("Order must be an integer")
-        if order < 0:
-            raise TypeError("Order must be positive")
-        if not np.isfinite(order):
-            raise ValueError("Order must be finite")
-
-        a1, a2 = self.GetPeakAmp(order)
-        return (np.abs(a1)**2 * self.GetPowerNorm(), np.abs(a2)**2 * self.GetPowerNorm())
